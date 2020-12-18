32fd497e16139c147131720a6a3a68b83df92caa
LizaSantanaC <santanaliza.c@gmail.com>
2020-12-18 15:33:24 -0600

    ALGORITMOS DE CAMINO MÁS CORTO - JAVA

--- src/PruebaAlgoritmos.java
+++ src/PruebaAlgoritmos.java
@@ -206,126 +206,60 @@ class AlgoritmoFloydWarshall {
 /* AUTOR DEL CÓDIGO BELLMAN FORD */
 
 class AlgoritmoBellmanFord {
-	public static long dist[];
-	public static long prev[];
-	public static LinkedList<DirectedEdge> edgesList = new LinkedList<DirectedEdge>();
-
-	public static class DirectedEdge {
-		protected int v;
-		protected int w; // edge
-		protected int weight; // weight;
-
-		public DirectedEdge(int v, int w, int weight) {
-			this.v = v;
-			this.w = w;
-			this.weight = weight;
-		}
-
-		public int from() {
-			return v;
-		}
-
-		public int to() {
-			return w;
-		}
-
-		public int weight() {
-			return weight;
-		}
-
-		public String toString() {
-			return v + "->" + w + " " + String.format("%d", weight);
-		}
-
-	}
-
-	public static class WeightableDiGraph {
-		protected int V; // vertex;
-		protected int E; // edges;
-		protected ArrayList<DirectedEdge>[] adj;
-
-		public WeightableDiGraph(int V) {
-			this.V = V;
-			this.E = 0;
-			adj = (ArrayList<DirectedEdge>[]) new ArrayList[V];
-			for (int v = 0; v < V; v++) {
-				adj[v] = new ArrayList<DirectedEdge>();
-			}
-
-		}
-
-		public int V() {
-			return this.V;
-		}
-
-		public int E() {
-			return this.E;
-		}
-
-		public void addEdge(int v, int w, int weight) {
-			this.E++;
-			DirectedEdge item = new DirectedEdge(v, w, weight);
-			adj[v].add(item);
-			edgesList.add(item);
-		}
-
-		public String toString() {
-			StringBuilder s = new StringBuilder();
-			String NEWLINE = System.getProperty("line.separator");
-			s.append(V + " vertices, " + E + " edges " + NEWLINE);
-			for (int v = 0; v < V; v++) {
-				s.append(String.format("%d: ", v));
-				for (DirectedEdge e : adj[v]) {
-					s.append(e + "  ");
-				}
-				s.append(NEWLINE);
-			}
-			return s.toString();
-		}
-
-		public Iterable<DirectedEdge> adj(int v) {
-			if (v < 0 || v >= V)
-				throw new IndexOutOfBoundsException();
-			return this.adj[v];
-		}
-
-	}
-
-	public static int BellmanFord(WeightableDiGraph g) {
-		Arrays.fill(dist, Long.MAX_VALUE);
-		Arrays.fill(prev, -1);
-		dist[0] = 0;
-		for (int i = 1; i < g.V() - 1; i++) {
-			for (DirectedEdge e : g.adj(i)) {
-
-				if (dist[e.to()] > dist[e.from()] + e.weight()) {
-					dist[e.to()] = dist[e.from()] + e.weight();
-					prev[e.to()] = e.to();
-				}
-			}
-		}
-
-		for (int i = 0; i < g.V(); i++) {
-			for (DirectedEdge e : g.adj(i)) {
-				if (dist[e.to()] > dist[e.from()] + e.weight())
-					return 1;
-			}
-		}
-		return 0;
-	}
+	private int distances[];
+    private int numberofvertices;
+    public static final int MAX_VALUE = 999;
+ 
+    public AlgoritmoBellmanFord(int numberofvertices){
+        this.numberofvertices = numberofvertices;
+        distances = new int[numberofvertices + 1];
+    }
+ 
+    public void BellmanFordEvaluation(int source, int adjacencymatrix[][]){
+        for (int node = 1; node <= numberofvertices; node++){
+            distances[node] = MAX_VALUE;
+        }
+ 
+        distances[source] = 0;
+        for (int node = 1; node <= numberofvertices - 1; node++){
+            for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++){
+                for (int destinationnode = 1; destinationnode <= numberofvertices; destinationnode++){
+                    if (adjacencymatrix[sourcenode][destinationnode] != MAX_VALUE){
+                        if (distances[destinationnode] > distances[sourcenode] 
+                                + adjacencymatrix[sourcenode][destinationnode])
+                            distances[destinationnode] = distances[sourcenode]
+                                + adjacencymatrix[sourcenode][destinationnode];
+                    }
+                }
+            }
+        }
+ 
+        for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++){
+            for (int destinationnode = 1; destinationnode <= numberofvertices; destinationnode++){
+                if (adjacencymatrix[sourcenode][destinationnode] != MAX_VALUE){
+                    if (distances[destinationnode] > distances[sourcenode]
+                           + adjacencymatrix[sourcenode][destinationnode])
+                        System.out.println("El gráfico contiene ciclo egde negativo");
+                }
+            }
+        }
+        for (int vertex = 1; vertex <= numberofvertices; vertex++){
+            System.out.println("Distancia de vertices " + source + " a "
+                      + vertex + " es " + distances[vertex]);
+        }
+    }
 }
 
 public class PruebaAlgoritmos {
 	public static void main(String[] args) {
 		AlgoritmoDijkstra dijkstra = new AlgoritmoDijkstra("abcdef");
 		AlgoritmoFloydWarshall floyWarshall = new AlgoritmoFloydWarshall();
-		AlgoritmoBellmanFord bellmanFord = new AlgoritmoBellmanFord();
-		long dist[];
-		long prev[];
 
 		// Menu
 		Scanner entrada1 = new Scanner(System.in);
 		Scanner stdin = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		
 		int e = 0;
 		boolean salir = false;
 		e = 0;
@@ -437,21 +371,32 @@ public class PruebaAlgoritmos {
 
 			case 3:
 				System.out.println("\t---------------AlgoritmoBellmanFord---------------\n");
-				int n, mn, v, w, weight;
-				Scanner sc = new Scanner(System.in);
-				n = sc.nextInt();
-				mn = sc.nextInt();
-				dist = new long[n];
-				prev = new long[n];
-				AlgoritmoBellmanFord.WeightableDiGraph g = new AlgoritmoBellmanFord.WeightableDiGraph(n);
-				for (int i = 0; i < mn; i++) {
-					v = sc.nextInt();
-					w = sc.nextInt();
-					weight = sc.nextInt();
-					g.addEdge(v - 1, w - 1, weight);
-				}
-
-				System.out.println(AlgoritmoBellmanFord.BellmanFord(g));
+				int numberofvertices = 0;
+		        int source;
+		        int MAX_VALUE = 999;
+		        Scanner scanner = new Scanner(System.in);
+		        System.out.println("Ingrese el número de vértices: ");
+		        numberofvertices = scanner.nextInt();
+		        int adjacencymatrix[][] = new int[numberofvertices + 1][numberofvertices + 1];
+		        System.out.println("Ingrese los valores a la matriz de adyacencia: ");
+		        System.out.println("\n Ejemplo:" + " \n 0 4 0 0 -1 0 \n" + " X X X X  X X \n");
+		        for (int sourcenode = 1; sourcenode <= numberofvertices; sourcenode++){
+		            for (int destinationnode = 1; destinationnode <= numberofvertices; destinationnode++){
+		                adjacencymatrix[sourcenode][destinationnode] = scanner.nextInt();
+		 	        if (sourcenode == destinationnode){
+		                    adjacencymatrix[sourcenode][destinationnode] = 0;
+		                    continue;
+		                }
+		                if (adjacencymatrix[sourcenode][destinationnode] == 0){
+		                    adjacencymatrix[sourcenode][destinationnode] = MAX_VALUE;
+		                }
+		            }
+		        }
+		        System.out.println("Ingrese el vértice inicial: ");
+		        source = scanner.nextInt();
+		        AlgoritmoBellmanFord bellmanford = new AlgoritmoBellmanFord(numberofvertices);
+		        bellmanford.BellmanFordEvaluation(source, adjacencymatrix);
+		        scanner.close();
 				break;
 
 			case 4:
@@ -463,6 +408,5 @@ public class PruebaAlgoritmos {
 			}
 		} while (!salir);
 		System.out.println("------------------------------------------------------------");
-
 	}
 }
