530e208324dadc4c47a66c35e6b9864743e46a38
LizaSantanaC <santanaliza.c@gmail.com>
2020-12-18 18:09:36 -0600

    ALGORITMOS DE CAMINO MÁS CORTO - PYTHON

---
+++ PYTHON/Algoritmos_Camino_Mas_Corto/BellmanFord.py
@@ -0,0 +1,51 @@
+#   AUTOR DE CÓDIGO BELLMAN-FORD: Elshad Karimov
+#   Copyright © 2021 AppMillers. All rights reserved.
+#   URL: https://drive.google.com/drive/folders/1VqXKBdSg3W3MkDHGmHqC1k4w0rBcRmHj
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = []
+        self.nodes = []
+
+    def add_edge(self, s, d, w):
+        self.graph.append( [s, d, w] )
+
+    def addNode(self, value):
+        self.nodes.append( value )
+
+    def print_solution(self, dist):
+        print( "Vertex Distance from Source" )
+        for key, value in dist.items():
+            print( '  ' + key, ' :    ', value )
+
+    def bellmanFord(self, src):
+        dist = {i: float( "Inf" ) for i in self.nodes}
+        dist[src] = 0
+
+        for _ in range( self.V - 1 ):
+            for s, d, w in self.graph:
+                if dist[s] != float( "Inf" ) and dist[s] + w < dist[d]:
+                    dist[d] = dist[s] + w
+
+        for s, d, w in self.graph:
+            if dist[s] != float( "Inf" ) and dist[s] + w < dist[d]:
+                print( "Graph contains negative cycle" )
+                return
+
+        self.print_solution( dist )
+g = Graph( 5 )
+g.addNode( "A" )
+g.addNode( "B" )
+g.addNode( "C" )
+g.addNode( "D" )
+g.addNode( "E" )
+g.add_edge( "A", "C", 6 )
+g.add_edge( "A", "D", 6 )
+g.add_edge( "B", "A", 3 )
+g.add_edge( "C", "D", 1 )
+g.add_edge( "D", "C", 2 )
+g.add_edge( "D", "B", 1 )
+g.add_edge( "E", "B", 4 )
+g.add_edge( "E", "D", 2 )
+g.bellmanFord( "E" )
